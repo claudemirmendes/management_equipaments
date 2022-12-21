b0d97d74dc96bd9a2c979465d94c11b824a4f395
Claudemir <claudemir>
2022-12-20 23:31:06 -0300

    initing project and login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManagementEquipaments::Application.config.secret_key_base = '877d6e5ef29cebfeefc6b7f2130ae4a4256928d9a870b4c53505e20d5f53294a5377bacce0533759a2e6626e102bccd7867ae3f957bc0ad73a199e6d3155c7b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ManagementEquipaments::Application.config.session_store :cookie_store, key: '_management_equipaments_session'
